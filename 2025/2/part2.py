3f073cf6a11699a50e8a34130d70551e5c56d346
tsi <timo42@proton.me>
2025-12-02 06:33:41 +0100

    Day 2: Add test.txt

---
+++ 2025/2/part2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pypy3
+import re
+
+ranges = open(0).read().replace('\n', '').strip().split(',')
+answer = 0
+for rng in ranges:
+    start, end = rng.split('-')
+    for num in range(int(start), int(end)+1):
+        if re.search(r'^(\d*)(\1)+$', str(num)):
+            answer += num
+print(answer)
