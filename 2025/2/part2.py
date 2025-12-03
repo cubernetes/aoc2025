f7cdf51a7bafee19341503fa4c74ef050b471a27
tsi <timo42@proton.me>
2025-12-03 08:08:06 +0100

    Day 2: Cleanup

--- 2025/2/part2.py
+++ 2025/2/part2.py
@@ -6,6 +6,6 @@ answer = 0
 for rng in ranges:
     start, end = rng.split('-')
     for num in range(int(start), int(end)+1):
-        if re.search(r'^(\d*)(\1)+$', str(num)):
+        if re.search(r'^(.+)\1+$', str(num)):
             answer += num
 print(answer)
