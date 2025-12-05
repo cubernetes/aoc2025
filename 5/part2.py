d2a75a2afeb0689f37e9f35a536f787febba8eb4
tsi <timo42@proton.me>
2025-12-05 06:29:23 +0100

    Day 5: Cleanup

--- 5/part2.py
+++ 5/part2.py
@@ -3,10 +3,8 @@ fresh = []
 for line in a.splitlines():
     s, e = line.split('-')
     fresh += [[int(s), int(e)]]
-
 fresh.sort()
 t = 0
-
 prev_end = -1
 for f in fresh:
     s, e = f
@@ -14,8 +12,6 @@ for f in fresh:
         s = prev_end + 1
     if s > e:
         continue
-    print(f'Adding range {s} {e}')
     t += e - s + 1
     prev_end = e
-
 print(t)
