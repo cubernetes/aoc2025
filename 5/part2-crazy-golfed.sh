ac43b83ddc316c0908f7d5917262a319367c93d9
tsi <timo42@proton.me>
2025-12-05 09:24:45 +0100

    Day 5: Improve and add golfed solutions

---
+++ 5/part2-crazy-golfed.sh
@@ -0,0 +1,1 @@
+grep -|sort -n|while IFS=- read s e;do echo $[(s=s>p?s:p+1)>e?0:(p=e,t+=e-s+1)];done
--- 5/part2-golfed.sh
+++ 5/part2-golfed.sh
@@ -1,1 +1,1 @@
-grep -|sort -n|(while IFS=- read s e;do((s=s<=p?p+1:s,s>e?0:(p=e,t+=e-s+1)));done;echo $t)
+grep -|sort -n|(while IFS=- read s e;do(((s=s>p?s:p+1)>e?0:(p=e,t+=e-s+1)))done;echo $t)
