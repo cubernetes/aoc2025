b7ded82bcf3159f729df70039836c70ecd3cdc2f
tsi <timo42@proton.me>
2025-12-06 09:14:04 +0100

    Day 6: Remove chmod +x and simplify a little

--- 6/part2.py
+++ 6/part2.py
@@ -1,11 +1,6 @@
 import numpy as np
 import re
-lines = list(map(list, open(0).read().strip().splitlines()))
-longest = -1
-for line in lines:
-    if len(line) > longest:
-        longest = len(line)
-lines[-1] = list((''.join(lines[-1])).ljust(longest))
+lines = list(map(list, open(0).read().splitlines()))
 lines = re.sub(r'\n *\n', r'\n\n', '\n'.join([''.join(x) for x in np.transpose(lines)]))
 problems = [x.splitlines() for x in lines.split('\n\n')]
 total = 0
